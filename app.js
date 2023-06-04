5d0e1647a0d86d526b3736c71b6abaea39058f24
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-06-04 12:10:46 +0700

    handling error

--- app.js
+++ app.js
@@ -12,4 +12,19 @@ app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use("/mahasiswa", mahasiswaRouter);
 
+app.use((req, res, next) => {
+	const error = new Error("Tidak Ditemukan");
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message,
+		},
+	});
+});
+
 module.exports = app;
