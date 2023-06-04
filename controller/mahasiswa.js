4008287e5c14140308baad6fab5b50cd7bd0e57d
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-06-04 18:12:24 +0700

    CRUD dengan ORM Sequelize

--- controller/mahasiswa.js
+++ controller/mahasiswa.js
@@ -3,22 +3,104 @@ const controller = {};
 
 controller.getAll = async function (req, res) {
 	try {
-		await model.mahasiswa.findAll().then((result) => {
-			if (result.length > 0) {
-				res.status(200).json({
-					message: "Get Method Mahasiswa",
-					data: result,
-				});
-			} else {
-				res.status(200).json({
-					message: "Tidak Ada Data",
-					data: [],
-				});
-			}
+		let mahasiswa = await model.mahasiswa.findAll();
+		if (mahasiswa.length > 0) {
+			res.status(200).json({
+				message: "Ambil Semua Data Mahasiswa",
+				data: mahasiswa,
+			});
+		} else {
+			res.status(200).json({
+				message: "Tidak Ada Data Mahasiswa",
+				data: [],
+			});
+		}
+	} catch (error) {
+		res.status(404).json({
+			message: error.message,
+		});
+	}
+};
+
+controller.getByNim = async function (req, res) {
+	try {
+		let mahasiswa = await model.mahasiswa.findAll({
+			where: { nim: req.params.nim },
+		});
+		if (mahasiswa.length > 0) {
+			res.status(200).json({
+				message: "Data Mahasiswa Ditemukan",
+				data: mahasiswa,
+			});
+		} else {
+			res.status(200).json({
+				message: "Data Mahasiswa Tidak Ditemukan",
+				data: [],
+			});
+		}
+	} catch (error) {
+		res.status(404).json({
+			message: error.message,
+		});
+	}
+};
+
+controller.post = async function (req, res) {
+	try {
+		let mahasiswa = await model.mahasiswa.create({
+			nim: req.body.nim,
+			nama: req.body.nama,
+			jurusan: req.body.jurusan,
+		});
+		res.status(201).json({
+			message: "Data Mahasiswa Berhasil Ditambahkan",
+			data: mahasiswa,
 		});
 	} catch (error) {
 		res.status(404).json({
-			message: error,
+			message: error.message,
+		});
+	}
+};
+
+controller.put = async function (req, res) {
+	try {
+		let mahasiswa = await model.mahasiswa.update(
+			{
+				nama: req.body.nama,
+				jurusan: req.body.jurusan,
+			},
+			{
+				where: {
+					nim: req.params.nim,
+				},
+			}
+		);
+		res.status(201).json({
+			message: "Data Mahasiswa Berhasil Diubah",
+		});
+	} catch (error) {
+		res.status(404).json({
+			message: error.message,
+		});
+	}
+};
+
+controller.delete = async function (req, res) {
+	try {
+		let mahasiswa = await model.mahasiswa.destroy({
+			where: {
+				nim: req.params.nim,
+			},
+		});
+
+		res.status(200).json({
+			message: "Data Mahasiswa Berhasil Dihapus",
+			data: mahasiswa,
+		});
+	} catch (error) {
+		res.status(404).json({
+			message: error.message,
 		});
 	}
 };
--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -5,60 +5,12 @@ const controller = require("../controller/index");
 
 router.get("/", controller.mahasiswa.getAll);
 
-router.get("/search", (req, res, next) => {
-	const nim = req.query.nim;
-	var sql = `SELECT * FROM mahasiswa WHERE NIM = ${nim}`;
-	db.query(sql, (err, result) => {
-		if (err) throw err;
-		if (result.length > 0) {
-			res.status(200).json({
-				message: "Mahasiswa Ditemukan",
-				data: result,
-			});
-		} else {
-			res.status(200).json({
-				message: "Mahasiswa Tidak Ditemukan",
-				data: result,
-			});
-		}
-	});
-});
+router.get("/:nim", controller.mahasiswa.getByNim);
 
-router.post("/", (req, res, next) => {
-	const nama = req.body.nama;
-	const nim = req.body.nim;
-	const jurusan = req.body.jurusan;
-	var sql = `INSERT INTO MAHASISWA VALUES ( DEFAULT, '${nama}', '${nim}', '${jurusan}')`;
-	db.query(sql, (err, result) => {
-		if (err) throw err;
-		res.status(200).json({
-			message: "Berhasil Tambah Data Mahasiswa",
-		});
-	});
-});
+router.post("/", controller.mahasiswa.post);
 
-router.put("/", (req, res, next) => {
-	const nama = req.body.nama;
-	const nim = req.body.nim;
-	const jurusan = req.body.jurusan;
-	var sql = `UPDATE MAHASISWA SET nama='${nama}' , jurusan='${jurusan}' WHERE nim='${nim}' `;
-	db.query(sql, (err, result) => {
-		if (err) throw err;
-		res.status(200).json({
-			message: "Data Mahasiswa Berhasil Diubah",
-		});
-	});
-});
+router.put("/:nim", controller.mahasiswa.put);
 
-router.delete("/:nim", (req, res, next) => {
-	const nim = req.params.nim;
-	var sql = `DELETE FROM MAHASISWA WHERE nim='${nim}' `;
-	db.query(sql, (err, result) => {
-		if (err) throw err;
-		res.status(200).json({
-			message: "Data Mahasiswa Berhasil Dihapus",
-		});
-	});
-});
+router.delete("/:nim", controller.mahasiswa.delete);
 
 module.exports = router;
